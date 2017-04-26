80647a842954ef394ad650cec92c3ce3e8b8b243
anaalta <suciuanamaria@yahoo.com>
2017-04-26 15:05:56 +0200

    added test for player names

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -2,8 +2,15 @@ require './app.rb'
 require 'capybara'
 
 describe Battle do
+
   it "checks homepage says 'Testing infrastructure working!'" do
-    visit('/') 
+    visit('/')
     expect(page).to have_content('Testing infrastructure working!')
   end
+
+ it 'expects player1 to be John' do
+   visit ('/players')
+   fill_in('Player1', :with => 'John')
+ end
+
 end
